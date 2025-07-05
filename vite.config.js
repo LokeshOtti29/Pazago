cee54635712cadfb39eb3da35e0b62e92baa6d77
LokeshOtti29 <lokeshotti456@gmail.com>
2025-07-05 21:55:53 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcssVite from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcssVite()],
+  base: process.env.VITE_BASE_PATH || "/weatherAgentApp-LokeshOtti",
 });
